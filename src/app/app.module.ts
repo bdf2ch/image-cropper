2f97d5b4192f8201fa819714dd6f32d3f0cd383a
bdf2ch <bdf2ch@gmail.com>
2019-11-25 23:44:07 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ImageCropperModule } from 'ngx-image-cropper';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageCropperComponent } from './components/image-cropper/image-cropper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageCropperComponent
   ],
   imports: [
     BrowserModule,
+    ImageCropperModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/components/image-cropper/image-cropper.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ImageCroppedEvent } from 'ngx-image-cropper';
+
+@Component({
+  selector: 'app-image-cropper',
+  templateUrl: './image-cropper.component.html',
+  styleUrls: ['./image-cropper.component.less']
+})
+export class ImageCropperComponent implements OnInit {
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+  }
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+  }
+  imageLoaded() {
+    // show cropper
+  }
+  cropperReady() {
+    // cropper ready
+  }
+  loadImageFailed() {
+    // show message
+  }
+}
